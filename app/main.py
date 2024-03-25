dbae9c937cbf292aca6ecfd8be9107199c909c5f
adityakiran1423 <adi.kiran@gmail.com>
2024-03-25 21:00:35 +0530

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -2,26 +2,32 @@ import socket
 import threading
 
 
+def resp_parser(request_message) -> str:
+    request_message.replace("\r\n","")
+    index=request_message.index("$")
+    length_request_message=int(request_message[index+1])
+    echo_message=request_message[index+2:]
+    return echo_message
 
-def threaded_redis_server(connection, reddis_pong_response):
+
+def threaded_redis_server(connection) -> None:
     while True:
+        redis_pong_response="+PONG\r\n"
         data=connection.recv(1024).decode(encoding="utf-8")
-        connection.send(reddis_pong_response.encode())
+        echo_message=resp_parser(data)
+        connection.send(echo_message.encode())
 
 
-def main():
+def main() -> None:
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     while True:
-        redis_pong_response="+PONG\r\n"
         (connection, address) = server_socket.accept()
-        thread = threading.Thread(target=threaded_redis_server, args=(connection, redis_pong_response,))
+        thread = threading.Thread(target=threaded_redis_server, args=(connection, ))
         thread.start()
-        # except BrokenPipeError:
-        #     sys.exit()
 
 
 if __name__ == "__main__":
