16af6188d3030c41b7c9a5c624faac7ae26e9781
adityakiran1423 <adi.kiran@gmail.com>
2024-03-25 14:52:40 +0530

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import sys
+
 
 
 def threaded_redis_server(connection, reddis_pong_response):
@@ -16,13 +16,12 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     while True:
-        try : 
-            redis_pong_response="+PONG\r\n"
-            (connection, address) = server_socket.accept()
-            thread = threading.Thread(target=threaded_redis_server, args=(connection, redis_pong_response,))
-            thread.start()
-        except BrokenPipeError:
-            sys.exit()
+        redis_pong_response="+PONG\r\n"
+        (connection, address) = server_socket.accept()
+        thread = threading.Thread(target=threaded_redis_server, args=(connection, redis_pong_response,))
+        thread.start()
+        # except BrokenPipeError:
+        #     sys.exit()
 
 
 if __name__ == "__main__":
