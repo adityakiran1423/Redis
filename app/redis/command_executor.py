92f6a32d11a5c8b4aca2a6dc26e931a52c60b3a1
adityakiran1423 <adi.kiran@gmail.com>
2024-03-31 17:37:31 +0530

    pass 6th stage

--- app/redis/command_executor.py
+++ app/redis/command_executor.py
@@ -1,29 +1,21 @@
 from app.redis.database import redis_database 
 
 def redis_set(request_message)-> None:
-    request_message.replace("\r\n","")
+    list=request_message.split("\r\n")
+
+    key=list[4]
+    value=list[6]
 
     mode='s'
-
-    index1=request_message.index("$")
-    index2=request_message.rfind("$")
-    for i in range(index1+1,index2):
-        if request_message[i]=="$":
-            key=request_message[i:index2]
-            break
-    value=request_message[index2:]
     _=redis_database(mode, key,value)
-    # redis_dictionary[key]=value
 
 
 def redis_get(request_message) -> str:
-    request_message.replace("\r\n","")
+    list=request_message.split("\r\n")
+
+    key=list[4]
 
     mode='g'
-
-    index2=request_message.rfind("$")
-    key=request_message[index2:]
-    
     value=redis_database(mode, key,"")
-    print(value)
+    
     return value
