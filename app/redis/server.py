233ade4b4994334c3662ce5a98441ad3a20c0973
adityakiran1423 <adi.kiran@gmail.com>
2024-03-31 18:14:07 +0530

    pass 6th stage

--- app/redis/server.py
+++ app/redis/server.py
@@ -6,7 +6,7 @@ def threaded_redis_server(connection) -> None:
         # echo_regex="\*2+\r\n\$\d+\r\necho\r\n\$\d+\r\n[a-zA-Z0-9]+\r\n"
 
         data=connection.recv(1024).decode(encoding="utf-8")
-        
+
         if data=="*1\r\n$4\r\nping\r\n":
             connection.send(redis_pong_response.encode())
 
@@ -16,6 +16,7 @@ def threaded_redis_server(connection) -> None:
 
         elif data.startswith("*3\r\n$3\r\nset\r\n"):
             echo_message=resp_parser(data)
+            print("This is from server.py in set elif block")
             connection.send(echo_message.encode())
 
         elif data.startswith("*2\r\n$3\r\nset\r\n"):
