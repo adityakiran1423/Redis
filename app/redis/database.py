173750d8be34f050add3ee185b811e89c0e4509c
adityakiran1423 <adi.kiran@gmail.com>
2024-03-31 11:30:04 +0530

    pass 6th stage

--- app/redis/database.py
+++ app/redis/database.py
@@ -5,7 +5,7 @@ def redis_database(mode, key, value)-> str:
         redis_dictionary[key]=value
         return "okay"
     else:
-        if key in redis_dictionary:
+        if key in redis_dictionary.keys():
             return redis_dictionary[key]
         else:
             return "$-1\r\n" 
