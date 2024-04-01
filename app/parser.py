d70c05cab737c8f5a9072d11b6eb3ae52ee95427
adityakiran1423 <adi.kiran@gmail.com>
2024-04-01 13:51:32 +0530

    pass 7th stage

--- app/parser.py
+++ app/parser.py
@@ -1,4 +1,6 @@
-from .redis.command_executor import redis_set, redis_get
+from .redis.command_executor import redis_set, redis_get, redis_set_with_expiry
+
+from datetime import datetime
 
 def resp_parser(request_message) -> str:
     if request_message.startswith("*2\r\n$4\r\necho\r\n"):
@@ -6,12 +8,18 @@ def resp_parser(request_message) -> str:
         index=request_message.rfind("$")
         response_message=request_message[index:]
 
-    elif request_message.startswith("*3\r\n$3\r\nset\r\n") or request_message.startswith("*5\r\n$3\r\nset\r\n"):
+    elif request_message.startswith("*3\r\n$3\r\nset\r\n"):
         response_message="+OK\r\n"
         redis_set(request_message)
 
+    elif request_message.startswith("*5\r\n$3\r\nset\r\n"):
+        response_message="+OK\r\n"
+        redis_set_with_expiry(request_message)
+
     elif request_message.startswith("*2\r\n$3\r\nget\r\n"):
-        response_message=redis_get(request_message)
+        get_time=datetime.now()
+        get_time.strftime('%H:%M:%S.%f%z')
+        response_message=redis_get(request_message, get_time)
         # print(response_message)
     
     return response_message
--- app/redis/command_executor.py
+++ app/redis/command_executor.py
@@ -1,4 +1,4 @@
-from app.redis.database import redis_dict, expiry_dict
+from app.redis.database import redis_dict, expiry_dict, set_time_dict
 
 from datetime import datetime
 
@@ -8,66 +8,47 @@ def redis_set(request_message):
     key=list[4]
     value=list[6]
 
-    # if request_message.startswith("*5\r\n$3\r\nset\r\n"):
-    #     expire_time=list[10]
-    #     set_time = datetime.now()
-    #     set_time.strftime('%H:%M:%S.%f%z')
-    #     redis_dict[key]=value
-    #     expiry_dict[key]=expire_time
-    #     return set_time, expire_time
-    # else:
     redis_dict[key]=value
 
 
-def redis_get(request_message) -> str:
+def redis_get(request_message, get_time) -> str:
     list=request_message.split("\r\n")
 
     key=list[4]
 
-    # if request_message.startswith("*3\r\n$3\r\nget\r\n"):
-    #if len(list)==11:
-    # if key in expiry_dict:
-    #     set_time, expire_time=redis_set(request_message)
-    #     print("in starts with 5 if")
-    #     current_time = datetime.now()
-    #     current_time.strftime('%H:%M:%S.%f%z')
-
-    #     time_delta=current_time-set_time
-    #     time_delta.total_seconds()
-    #     time_delta=time_delta.total_seconds()*1000
-
-    #     print(f"set_time is {set_time}")
-    #     print(f"current_time is {current_time}")
-    #     print(f"expiry is {expire_time}")
-    #     print(f"time_delta is {time_delta}")
-
-    #     status=expiry_handler(set_time, current_time, expire_time,key)
-    #     print(status)
-    #     if status:
-    #         return "$-1\r\n"
-    #     else:
-    #         if key in redis_dict:
-    #             print("printing from status false else")
-    #             return f"${len(redis_dict[key])}\r\n{redis_dict[key]}\r\n"
-    #         else:
-    #             return "$-1\r\n"
-
-    # else:
-    #     print("in main else")
     if key in redis_dict:
         print("returning from main else")
         return f"${len(redis_dict[key])}\r\n{redis_dict[key]}\r\n"
+    
+    elif key in expiry_dict:
+        status=expiry_checker(set_time_dict[key], get_time, expiry_dict[key])
+        if status:
+            return "$-1\r\n"
+        else:
+            if key in redis_dict:
+                print("returning from main else")
+                return f"${len(redis_dict[key])}\r\n{redis_dict[key]}\r\n"
+            
     else:
         return "$-1\r\n"
 
-def expiry_handler(set_time, current_time, expire_time,key)->bool:
-    time_delta=current_time-set_time
-    print(f"set_time is {set_time}")
-    print(f"current_time is {current_time}")
-    print(f"expiry is {expire_time}")
-    print(f"time_delta is {time_delta}") 
-    if time_delta>expire_time:
-        del redis_dict[key]
+def redis_set_with_expiry(request_message):
+    list=request_message.split("\r\n")
+    
+    key=list[4]
+    value=list[6]
+    expiry=list[10]
+    
+    redis_dict[key]=value
+    expiry_dict[key]=expiry
+    set_time=datetime.now()
+    set_time.strftime('%H:%M:%S.%f%z')
+    set_time_dict[key]=set_time
+
+
+def expiry_checker(set_time, get_time, expiry_time)->bool:
+    time_delta=get_time-set_time
+    if time_delta>expiry_time:
         return True
     else:
         return False
