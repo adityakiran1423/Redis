9a758624f5b891033725698fceca5a8c15a2c1cb
adityakiran1423 <adi.kiran@gmail.com>
2024-04-01 17:56:26 +0530

    format black

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@ import threading
 
 from .redis.server import threaded_redis_server
 
+
 def main() -> None:
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     while True:
         (connection, address) = server_socket.accept()
-        thread = threading.Thread(target=threaded_redis_server, args=(connection, ))
+        thread = threading.Thread(target=threaded_redis_server, args=(connection,))
         thread.start()
 
 
--- app/parser.py
+++ app/parser.py
@@ -2,24 +2,25 @@ from .redis.command_executor import redis_set, redis_get, redis_set_with_expiry
 
 from datetime import datetime
 
+
 def resp_parser(request_message) -> str:
     if request_message.startswith("*2\r\n$4\r\necho\r\n"):
-        request_message.replace("\r\n","")
-        index=request_message.rfind("$")
-        response_message=request_message[index:]
+        request_message.replace("\r\n", "")
+        index = request_message.rfind("$")
+        response_message = request_message[index:]
 
     elif request_message.startswith("*3\r\n$3\r\nset\r\n"):
-        response_message="+OK\r\n"
+        response_message = "+OK\r\n"
         redis_set(request_message)
 
     elif request_message.startswith("*5\r\n$3\r\nset\r\n"):
-        response_message="+OK\r\n"
+        response_message = "+OK\r\n"
         redis_set_with_expiry(request_message)
 
     elif request_message.startswith("*2\r\n$3\r\nget\r\n"):
-        get_time=datetime.now()
-        get_time.strftime('%H:%M:%S.%f%z')
-        response_message=redis_get(request_message, get_time)
+        get_time = datetime.now()
+        get_time.strftime("%H:%M:%S.%f%z")
+        response_message = redis_get(request_message, get_time)
         # print(response_message)
-    
+
     return response_message
