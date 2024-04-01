0bf69d38f937711d8f81061cf8460f7ec8e6242c
adityakiran1423 <adi.kiran@gmail.com>
2024-04-01 17:53:48 +0530

    refactor and polish

--- app/redis/server.py
+++ app/redis/server.py
@@ -1,25 +1,26 @@
 from app.parser import resp_parser
 
+
 def threaded_redis_server(connection) -> None:
     while True:
-        redis_pong_response="+PONG\r\n"
+        redis_pong_response = "+PONG\r\n"
         # echo_regex="\*2+\r\n\$\d+\r\necho\r\n\$\d+\r\n[a-zA-Z0-9]+\r\n"
 
-        data=connection.recv(1024).decode(encoding="utf-8")
+        data = connection.recv(1024).decode(encoding="utf-8")
 
-        if data=="*1\r\n$4\r\nping\r\n":
+        if data == "*1\r\n$4\r\nping\r\n":
             connection.send(redis_pong_response.encode())
 
         elif data.startswith("*2\r\n$4\r\necho\r\n"):
-            echo_message=resp_parser(data)
+            echo_message = resp_parser(data)
             connection.send(echo_message.encode())
 
         elif data.startswith("*3\r\n$3\r\nset\r\n") or data.startswith("*5\r\n$3\r\nset\r\n"):
-            echo_message=resp_parser(data)
+            echo_message = resp_parser(data)
             # print("This is from server.py in set elif block")
             connection.send(echo_message.encode())
 
         elif data.startswith("*2\r\n$3\r\nget\r\n"):
-            echo_message=resp_parser(data)
+            echo_message = resp_parser(data)
             # print(echo_message)
             connection.send(echo_message.encode())
